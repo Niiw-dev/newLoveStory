bc8e82483713c842e4992d09d41d03eca6b1ae5c
felipe.alarcon <felipe.alarcon@savne.net>
2025-09-30 13:25:26 -0500

    Se ajustan migraciones y base de datos base

--- estructura/models.py
+++ estructura/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import time, date, timedelta
+from datetime import time, date
 from django.core.exceptions import ValidationError
 
 class Servicio(models.Model):
     tipo_de_servicio = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio_base = models.DecimalField(max_digits=10, decimal_places=2)
-    duracion = models.DurationField(default=timedelta(hours=1))  # default para migraciones
+    duracion = models.IntegerField(default=60)
     categoria = models.CharField(
         max_length=50,
         choices=[('boda', 'Boda'), ('retrato', 'Retrato'), ('empresarial', 'Empresarial')],
@@ -21,7 +21,7 @@ class Paquete(models.Model):
     nombre_paquete = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    duracion = models.DurationField(default=timedelta(hours=1))
+    duracion = models.IntegerField(default=60)
 
     def __str__(self):
         return self.nombre_paquete
--- sistema/settings.py
+++ sistema/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'love_story',
-        'USER': 'lstory',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3307'
